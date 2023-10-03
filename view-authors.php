dfaa3ea72af961caafe5ffcb6e3c586c47d033d8
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-02 22:10:47 -0500

    Create view-authors.php

---
+++ view-authors.php
@@ -0,0 +1,27 @@
+<h1>Authors</h1>
+<div class="table-responsive">
+  <table class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Date of Birht</th>
+          <th>Nationality</th> 
+        </tr>
+      </thead>
+      <tbody>
+<?php
+while($authors = $authors->fetch_assoc()) {
+  ?>
+  <tr>
+    <td><?php echo $authors['authors_id']; ?></td>
+    <td><?php echo $authors['authors_name']; ?></td>
+    <td><?php echo $authors['birth_date']; ?></td>
+    <td><?php echo $authors['nationality']; ?></td>
+  </tr>
+  <?php
+}
+?>
+      </tbody>
+  </table>
+</div>
