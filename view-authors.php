7e1c07c2c9fd1c2d39e919de2402cabbb59a8be6
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-02 22:27:28 -0500

    Update view-authors.php

--- view-authors.php
+++ view-authors.php
@@ -12,14 +12,14 @@
       <tbody>
 <?php
 while($authors = $authors->fetch_assoc()) {
-  ?>
+ ?>
   <tr>
     <td><?php echo $authors['authors_id']; ?></td>
     <td><?php echo $authors['authors_name']; ?></td>
     <td><?php echo $authors['birth_date']; ?></td>
     <td><?php echo $authors['nationality']; ?></td>
   </tr>
-  <?php
+<?php
 }
 ?>
       </tbody>
