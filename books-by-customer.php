2bed5d21fcfc6b07f34226c82ff59f54761818e6
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-03 11:07:15 -0500

    Update books-by-customer.php

--- books-by-customer.php
+++ books-by-customer.php
@@ -4,7 +4,7 @@ require_once("model-books-by-customer.php");
 
 $pageTitle = "Books by Customer";
 include "view-header.php";
-$books = selectBooksByCustomer($_GET['id');
+$books = selectBooksByCustomers($_GET['id');
 include "view-books-by-customer.php";
 include "view-footer.php";
 ?>
